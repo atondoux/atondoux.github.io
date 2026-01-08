e746f105514d4c600ff73e643d87be76eec8c533
Aurélien <atondoux@gmail.com>
2026-01-08 23:25:38 +0100

    :lipstick: improve UI details

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -11,7 +11,7 @@ defineProps<{
     <UNavigationMenu
       :items="links"
       variant="link"
-      color="neutral"
+      color="primary"
       class="bg-muted/80 backdrop-blur-sm rounded-full px-2 sm:px-4 border border-muted/50 shadow-lg shadow-neutral-950/5"
       :ui="{
         link: 'px-2 py-1',
